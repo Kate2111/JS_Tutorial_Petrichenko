3e45d0555703fe3354bb3dfb650bcab0cb4639ad
Kate <noskova2111@mail.ru>
2022-06-20 21:44:51 +0300

    lesson 47

--- js/lessons.js
+++ js/lessons.js
@@ -1018,3 +1018,105 @@ let end = new Date();
 
 alert(`Цикл отработал за ${end - start} миллисекунд`); 
 */
+
+
+
+//      Урок 45. Функции конструкторы
+
+/* 
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    }
+} 
+
+User.prototype.exit = function() {
+    console.log(`Пользователь ${this.name} ушел`)
+};
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.exit();
+alex.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex); 
+*/
+
+
+
+//         Урок 47. Контекст вызова. This
+
+
+// 1) Обычная функция: this = window, но если use strict - underfined
+/* 
+function showThis(a, b) {
+    console.log(this);
+    function sum() {
+        console.log(this);
+        return a + b;
+    }
+
+    console.log(sum());
+}
+
+showThis(4, 5);
+ */
+
+// 2) Контекст у методов объекта это будет - сам объект 
+/* 
+const obj = {
+    a: 20,
+    b: 15,
+    sum: function() {
+        function shout() {
+            console.log(this);
+        }
+        shout();
+    }
+}
+
+obj.sum();
+ */
+
+// 3) this  в конструкторах и классах - эьто новый экземпляр объекта
+/* 
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+} 
+
+let ivan = new User('Ivan', 23);
+
+ */
+
+// 4) Ручная привязка this: call, apply, bind
+/* 
+function sayName(surname) {
+    console.log(this);
+    console.log(this.name + surname);
+}
+
+const user = {
+    name: 'John'
+};
+
+sayName.call(user, 'Smith');
+sayName.apply(user, ['Smith']);
+
+function count(num) {
+    return this*num;
+}
+
+const double = count.bind(2);
+console.log(double(3));
+console.log(double(15));
+  */
