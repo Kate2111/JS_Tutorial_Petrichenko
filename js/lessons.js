7b5aba284ad85252b595ce96867b1d7a406194c8
Kate <noskova2111@mail.ru>
2022-07-04 22:02:26 +0300

    lesson 49

--- js/lessons.js
+++ js/lessons.js
@@ -1052,7 +1052,7 @@ console.log(alex);
 
 
 
-//         Урок 47. Контекст вызова. This
+//         Урок 46. Контекст вызова. This
 
 
 // 1) Обычная функция: this = window, но если use strict - underfined
@@ -1120,3 +1120,61 @@ const double = count.bind(2);
 console.log(double(3));
 console.log(double(15));
   */
+
+
+
+//          Урок 47. Классы
+
+/* 
+class Rectangle {
+    constructor(height, width) {
+        this.height = height; // с помощью this мы обращаем к экземпляру нового созданного объект, в каждый новый объект мы записываем свойство height
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+ */
+/* 
+class ColoredRecrangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);  // вызываем свойство объекта родителя(Rectangle, super - всегда должно быть в начале конструктора первой строчкой
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`); 
+    }
+}
+
+
+const div = new ColoredRecrangleWithText(25, 10, 'Hello word', 'red');
+div.showMyProps();
+console.log(div.calcArea()); 
+*/
+
+/* const square = new Rectangle(10, 10);
+const long = new Rectangle(20, 100);
+
+console.log(square.calcArea());
+console.log(long.calcArea()); */
+
+
+
+//          Урок 49. Rest оператор и параметры по умолчанию
+/* 
+const log = function(a, b, ...rest) {
+    console.log(a, b, rest);
+}
+
+log('basic', 'rest', 'operator', 'usage');
+
+function calcOrDouble(number, basis = 2) {
+    console.log(number * basis);
+}
+
+calcOrDouble(3);
+ */
